4d5f068ae8b393a9dbd705df81048d3c9360ee61
master.cokorda@gmail.com <master.cokorda@gmail.com>
2024-12-15 20:49:18 +0800

    menambahkan fitur membaca suhu menggunakan dht

--- led/led.ino
+++ led/led.ino
@@ -1,94 +1,108 @@
 #include <WiFi.h>
 #include <WebServer.h>
+#include <DHT.h>
+
+// WiFi credentials
 const char* ssid = "Samsung Galaxy A55";
 const char* password = "123456789";
+
+// Web server
 WebServer server(80);
-// 19 18 5 4
-// bawaan led esp2
+
+// LED Pins
 const int espLed = 2;
 const int led1 = 19;
 const int led2 = 18;
 const int led3 = 5;
 const int led4 = 22;
 
+// DHT Sensor
+#define DHTPIN 4     // Pin connected to the DHT sensor
+#define DHTTYPE DHT11 // DHT11 or DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+// LED States
 bool lampupertamastat = false;
 bool lampukeduastat = false;
 bool lampuketigastat = false;
 bool lampukeempatstat = false;
 
 void setup() {
-
-  // put your setup code here, to run once:
   Serial.begin(115200);
+
+  // Initialize LED pins
   pinMode(led1, OUTPUT);
   pinMode(espLed, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
 
+  // Initialize DHT sensor
+  dht.begin();
+
+  // Connect to WiFi
   WiFi.begin(ssid, password);
-  Serial.print("lagi koneksii....");
-  // ketika tidak konek
+  Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(200);
     Serial.print(".");
     digitalWrite(espLed, LOW);
   }
-  // ketika konek
   Serial.println("\nConnected to WiFi!");
   digitalWrite(espLed, HIGH);
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  // Define Web Server Endpoints
   server.on("/lampupertama", HTTP_GET, getlampupertama);
   server.on("/lampukedua", HTTP_GET, getlampukedua);
   server.on("/lampuketiga", HTTP_GET, getlampuketiga);
   server.on("/lampukeempat", HTTP_GET, getlampukeempat);
-
 
   server.on("/lampupertama", HTTP_POST, setlampupertama);
   server.on("/lampukedua", HTTP_POST, setlampukedua);
   server.on("/lampuketiga", HTTP_POST, setlampuketiga);
   server.on("/lampukeempat", HTTP_POST, setlampukeempat);
 
+  // Endpoint for DHT data
+  server.on("/dht", HTTP_GET, getDHTData);
+
+  // Start the server
   server.begin();
-  Serial.println("server started..");
+  Serial.println("Server started...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   server.handleClient();
 }
-// Metod post
+
+// POST Methods
 void setlampupertama() {
   lampupertamastat = !lampupertamastat;
-  // Kalau true high, false low.
   digitalWrite(led1, lampupertamastat ? HIGH : LOW);
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampupertamastat ? "ON" : "OFF");
 }
 void setlampukedua() {
   lampukeduastat = !lampukeduastat;
-  // Kalau true high, false low.
   digitalWrite(led2, lampukeduastat ? HIGH : LOW);
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampukeduastat ? "ON" : "OFF");
 }
 void setlampuketiga() {
   lampuketigastat = !lampuketigastat;
-  // Kalau true high, false low.
   digitalWrite(led3, lampuketigastat ? HIGH : LOW);
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampuketigastat ? "ON" : "OFF");
 }
 void setlampukeempat() {
   lampukeempatstat = !lampukeempatstat;
-  // Kalau true high, false low.
   digitalWrite(led4, lampukeempatstat ? HIGH : LOW);
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampukeempatstat ? "ON" : "OFF");
 }
-// metod get
+
+// GET Methods
 void getlampupertama() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampupertamastat ? "ON" : "OFF");
@@ -104,4 +118,25 @@ void getlampuketiga() {
 void getlampukeempat() {
   server.sendHeader("Access-Control-Allow-Origin", "*");
   server.send(200, "text/plain", lampukeempatstat ? "ON" : "OFF");
+}
+
+// DHT Data Endpoint
+void getDHTData() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  // Check if readings are valid
+  if (isnan(temperature) || isnan(humidity)) {
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(500, "text/plain", "Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Send JSON response
+  String json = "{";
+  json += "\"temperature\": " + String(temperature) + ",";
+  json += "\"humidity\": " + String(humidity);
+  json += "}";
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "application/json", json);
 }
--- script.js
+++ script.js
@@ -8,106 +8,140 @@ const led2gam = document.getElementById("ledgamdua");
 const led3gam = document.getElementById("ledgamtiga");
 const led4gam = document.getElementById("ledgam4");
 
-const endpoint = "http://192.168.171.94";
+const endpoint = "http://192.168.37.94";
 const span = document.querySelector("span");
 
 function getlampupertama() {
   fetch(endpoint + "/lampupertama", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if( result == "ON" ){
-      led1Butt.style.backgroundColor = "red";
-      led1gam.src = "./image/led-on.png";
-      span.style.color = "red";
-    }
-    else{
-      led1Butt.style.backgroundColor = "blue";
-      led1gam.src = "./image/led-off.png";
-      span.style.color = "blue";
-    }
+    // ambil endpoint/lampupertama
+    method: "GET",
   })
+  // jika hasil ada on of maka..
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led1Butt.style.backgroundColor = "red";
+        led1gam.src = "./image/led-on.png";
+        span.style.color = "red";
+      } else {
+        led1Butt.style.backgroundColor = "blue";
+        led1gam.src = "./image/led-off.png";
+        span.style.color = "blue";
+      }
+    });
 }
 function setlampupertama() {
   fetch(endpoint + "/lampupertama", {
-    method: "POST"
-  }).then(response => response.text()).then(() => location.reload())
-
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then(() => location.reload());
 }
-
 
 function getlampukedua() {
   fetch(endpoint + "/lampukedua", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON"){
-      led2Butt.style.backgroundColor = "red";
-      led2gam.src = "./image/led-on.png";
-      span.style.color = "red";
-    }
-    else{
-      led2Butt.style.backgroundColor = "blue";
-      led2gam.src = "./image/led-off.png";
-      span.style.color = "blue";
-    }
+    method: "GET",
   })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led2Butt.style.backgroundColor = "red";
+        led2gam.src = "./image/led-on.png";
+        span.style.color = "red";
+      } else {
+        led2Butt.style.backgroundColor = "blue";
+        led2gam.src = "./image/led-off.png";
+        span.style.color = "blue";
+      }
+    });
 }
 function setlampukedua() {
   fetch(endpoint + "/lampukedua", {
-    method: "POST"
-  }).then(response => response.text()).then(() => location.reload())
-
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then(() => location.reload());
 }
-
 
 function getlampuketiga() {
   fetch(endpoint + "/lampuketiga", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON"){
-      led3Butt.style.backgroundColor = "red";
-      led3gam.src = "./image/led-on.png";
-      span.style.color = "red";
-    }
-    else{
-      led3Butt.style.backgroundColor = "blue";
-      led3gam.src = "./image/led-off.png";
-      span.style.color = "blue";
-    }
+    method: "GET",
   })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led3Butt.style.backgroundColor = "red";
+        led3gam.src = "./image/led-on.png";
+        span.style.color = "red";
+      } else {
+        led3Butt.style.backgroundColor = "blue";
+        led3gam.src = "./image/led-off.png";
+        span.style.color = "blue";
+      }
+    });
 }
 function setlampuketiga() {
   fetch(endpoint + "/lampuketiga", {
-    method: "POST"
-  }).then(response => response.text()).then(() => location.reload())
-
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then(() => location.reload());
 }
 
 function getlampukeempat() {
   fetch(endpoint + "/lampukeempat", {
-    method: "GET"
-  }).then(response => response.text()).then(result => {
-    if(result == "ON"){
-      led4Butt.style.backgroundColor = "red";
-      led4gam.src = "./image/led-on.png";
-      span.style.color = "red";
-    }
-    else{
-      led4Butt.style.backgroundColor = "blue";
-      led4gam.src = "./image/led-off.png";
-      span.style.color = "blue";
-    }
+    method: "GET",
   })
+    .then((response) => response.text())
+    .then((result) => {
+      if (result == "ON") {
+        led4Butt.style.backgroundColor = "red";
+        led4gam.src = "./image/led-on.png";
+        span.style.color = "red";
+      } else {
+        led4Butt.style.backgroundColor = "blue";
+        led4gam.src = "./image/led-off.png";
+        span.style.color = "blue";
+      }
+    });
 }
 function setlampukeempat() {
   fetch(endpoint + "/lampukeempat", {
-    method: "POST"
-  }).then(response => response.text()).then(() => location.reload())
-
+    method: "POST",
+  })
+    .then((response) => response.text())
+    .then(() => location.reload());
 }
 
+// DHT
+const temperatureSpan = document.getElementById("temperature");
+const humiditySpan = document.getElementById("humidity");
 
+// Fungsi untuk membaca data DHT
+function getDHTData() {
+  fetch(endpoint + "/dht", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Perbarui elemen dengan data suhu dan kelembaban
+      temperatureSpan.textContent = data.temperature.toFixed(1);
+      humiditySpan.textContent = data.humidity.toFixed(1);
+    })
+    .catch((error) => {
+      console.error("Gagal membaca data DHT:", error);
+      temperatureSpan.textContent = "--";
+      humiditySpan.textContent = "--";
+    });
+}
 
-getlampupertama()
-getlampukedua()
-getlampuketiga()
-getlampukeempat()
+// Panggil fungsi getDHTData setiap 0.5 detik
+setInterval(getDHTData, 500);
+
+// Panggil fungsi untuk mendapatkan data awal
+getDHTData();
+
+getlampupertama();
+getlampukedua();
+getlampuketiga();
+getlampukeempat();
